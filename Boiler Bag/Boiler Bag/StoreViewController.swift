3f390c0f2f1199bcb5de53b3cc559629f50225d0
Kunwar Shani <kunwar.sahni01@gmail.com>
2019-09-14 16:20:11 -0400

    Added Firebase and other ui

---
+++ Boiler Bag/Boiler Bag/StoreViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StoreViewController.swift
+//  Boiler Bag
+//
+//  Created by Kunwar Sahni on 9/14/19.
+//  Copyright © 2019 hello world. All rights reserved.
+//
+
+import UIKit
+
+class StoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
---
+++ Boiler Bag/Pods/Target Support Files/Pods-Boiler BagTests/Pods-Boiler BagTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Boiler_BagTests : NSObject
+@end
+@implementation PodsDummy_Pods_Boiler_BagTests
+@end
