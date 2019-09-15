2d53d35845f45122154eb9d1fed2d1501aedd652
Kunwar Shani <kunwar.sahni01@gmail.com>
2019-09-14 22:57:23 -0400

    working on Firebase Retrival
    idk

--- Boiler Bag/Boiler Bag/StoreProfileViewController.swift
+++ Boiler Bag/Boiler Bag/StoreProfileViewController.swift
@@ -7,16 +7,38 @@
 //
 
 import UIKit
+import Firebase
 
 class StoreProfileViewController: UIViewController{
-
+    @IBOutlet weak var tblStore: UITableView!
+    
     @IBOutlet weak var storeImage: UIImageView!
     var storeNameDetail: String?
-
+    var refStores: DatabaseReference!
     @IBOutlet weak var storeName: UILabel!
+    
+    var storeList = [storeModel]()
     override func viewDidLoad() {
         super.viewDidLoad()
-        //print(storeNameDetail)
+        refStores = Database.database().reference().child("8sLfEv4TJHGvcaPUl8LLfeIE_R-_dTCwCszPHX7OrYo").child("Markets")
+        refStores.observe(DataEventType.value, with:{ (snapshot) in
+            if snapshot.childrenCount>0{
+                self.storeList.removeAll()
+                for stores in snapshot.children.allObjects as![DataSnapshot]{
+                    let storesObject = stores.value as? [String: AnyObject]
+                    let storesCategory = storesObject?["Category"]
+                    let storesId = storesObject?["ID"]
+                    let storesName = storesObject?["Name"]
+                    let storesPrice = storesObject?["Price"]
+                    let storesPicture = storesObject?["picture"]
+                    
+                    let store = storeModel(category: storesCategory as! String?, id: storesId as! String?, name: storesName as! String?, picture: storesPicture as! String?, price: storesPicture as! String?)
+                    
+                    self.storeList.append(store)
+                }
+                self.tblStore.reloadData()
+            }
+        })
     }
     
     
--- Boiler Bag/Boiler Bag/storeModel.swift
+++ Boiler Bag/Boiler Bag/storeModel.swift
@@ -10,5 +10,16 @@ import Foundation
 import UIKit
 
 class storeModel{
-    var category: String!
+    var category: String?
+    var id: String?
+    var name: String?
+    var picture: String?
+    var price: String?
+    
+    init(category: String?, id: String?, name: String?, picture: String?, price: String?){
+        self.id = id
+        self.name = name
+        self.picture = picture
+        self.price = price
+    }
 }
